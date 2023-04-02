23a6115e4316634512df35be96668bfcaf238ee0
Zack Dowd <zdowd2796@gmail.com>
2023-04-02 12:00:00 -0600

    base nav meshes for enemies added

--- Assets/Zombies/ZombieController.cs
+++ Assets/Zombies/ZombieController.cs
@@ -1,46 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieController : MonoBehaviour
 {
+    [SerializeField] private GameObject target;
     private Animator anim;
+    private NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
+        anim.SetBool("IsWalking", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    anim.SetBool("IsWalking", true);
-        //} else
-        //{
-        //    anim.SetBool("IsWalking", false);
-        //}
+        agent.SetDestination(target.transform.position);
 
-        //if (Input.GetKey(KeyCode.R))
-        //{
-        //    anim.SetBool("IsRunning", true);
-        //} else
-        //{
-        //    anim.SetBool("IsRunning", false);
-        //}       
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //    anim.SetBool("IsAttacking", true);
-        //} else
-        //{
-        //    anim.SetBool("IsAttacking", false);
-        //}
+        if(agent.remainingDistance > agent.stoppingDistance)
+        {
+            anim.SetBool("IsWalking", true);
+            anim.SetBool("IsAttacking", false);
+        } else
+        {
+            anim.SetBool("IsWalking", false);
+            anim.SetBool("IsAttacking", true);
+        }
+        /*if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("IsWalking", true);
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+        }
 
-        //if(Input.GetKeyDown(KeyCode.D))
-        //{
-        //    anim.SetTrigger("Death");
-        //}
+        if (Input.GetKey(KeyCode.R))
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("IsAttacking", true);
+        }
+        else
+        {
+            anim.SetBool("IsAttacking", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetTrigger("Death");
+        }*/
 
 
     }
