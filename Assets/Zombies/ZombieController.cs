4d3e815abc0d96bfb5c94060e09be753c2c300ea
Zack Dowd <zdowd2796@gmail.com>
2023-04-03 11:37:25 -0600

    zombie animation states working properly

--- Assets/Zombies/ZombieController.cs
+++ Assets/Zombies/ZombieController.cs
@@ -6,62 +6,122 @@ using UnityEngine.AI;
 public class ZombieController : MonoBehaviour
 {
     [SerializeField] private GameObject target;
+    [SerializeField] private float walkingSpeed;
+    [SerializeField] private float runningSpeed;
     private Animator anim;
     private NavMeshAgent agent;
+
+    private enum State { IDLE, WANDER, ATTACK, CHASE, DEAD };
+    private State state = State.IDLE;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
+    }
 
-        anim.SetBool("IsWalking", true);
+
+    private void TurnOffTriggers()
+    {
+        anim.SetBool("IsWalking", false);
+        anim.SetBool("IsAttacking", false);
+        anim.SetBool("IsRunning", false);
+    }
+
+    private float DistanceToPlayer()
+    {
+        return Vector3.Distance(target.transform.position, transform.position);
+    }
+
+    private bool CanSeePlayer()
+    {
+        if (DistanceToPlayer() < 10)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool AbandonChase()
+    {
+        if (DistanceToPlayer() > 20)
+        {
+            return true;
+        }
+        return false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.transform.position);
+        switch (state)
+        {
+            case State.IDLE:
+                if (CanSeePlayer())
+                {
+                    state = State.CHASE;
+                }
+                else if (Random.Range(0, 5000) < 5)
+                {
+                    state = State.WANDER;
+                }
+                break;
+            case State.WANDER:
+                if (!agent.hasPath)
+                {
+                    float newX = this.transform.position.x + Random.Range(-5, 5);
+                    float newZ = this.transform.position.z + Random.Range(-5, 5);
+                    float newY = Terrain.activeTerrain.SampleHeight(new Vector3(newX, 0, newZ));
+                    Vector3 dest = new Vector3(newX, newY, newZ);
+                    agent.SetDestination(dest);
+                    agent.stoppingDistance = 0;
+                    agent.speed = walkingSpeed;
+                    TurnOffTriggers();
+                    anim.SetBool("IsWalking", true);
+                }
+                if (CanSeePlayer())
+                {
+                    state = State.CHASE;
+                } else if(Random.Range(0, 5000) < 5)
+                {
+                    state = State.IDLE;
+                    TurnOffTriggers();
+                    agent.ResetPath();
+                }
+                break;
+            case State.CHASE:
+                agent.SetDestination(target.transform.position);
+                agent.stoppingDistance = 3;
+                agent.speed = runningSpeed;
+                TurnOffTriggers();
+                anim.SetBool("IsRunning", true);
 
-        if(agent.remainingDistance > agent.stoppingDistance)
-        {
-            anim.SetBool("IsWalking", true);
-            anim.SetBool("IsAttacking", false);
-        } else
-        {
-            anim.SetBool("IsWalking", false);
-            anim.SetBool("IsAttacking", true);
-        }
-        /*if (Input.GetKey(KeyCode.W))
-        {
-            anim.SetBool("IsWalking", true);
-        }
-        else
-        {
-            anim.SetBool("IsWalking", false);
-        }
+                if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+                {
+                    state = State.ATTACK;
+                }
 
-        if (Input.GetKey(KeyCode.R))
-        {
-            anim.SetBool("IsRunning", true);
-        }
-        else
-        {
-            anim.SetBool("IsRunning", false);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            anim.SetBool("IsAttacking", true);
-        }
-        else
-        {
-            anim.SetBool("IsAttacking", false);
-        }
+                if (AbandonChase())
+                {
+                    state = State.WANDER;
+                    agent.ResetPath();
+                }
 
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            anim.SetTrigger("Death");
-        }*/
+                break;
+            case State.ATTACK:
+                TurnOffTriggers();
+                anim.SetBool("IsAttacking", true);
+                transform.LookAt(target.transform.position + new Vector3(0, -1, 0));
+                if (DistanceToPlayer() > agent.stoppingDistance + 2)
+                {
+                    state = State.CHASE;
+                }
+                break;
+            case State.DEAD:
+                break;
+        }
 
 
     }
